f0c68beaea09b1114a318838bdbcdda6c3dbb966
Mariano Sciacco <34033090+Maxelweb@users.noreply.github.com>
2018-04-20 01:45:25 +0200

    Final fix

--- _updates.php
+++ _updates.php
@@ -2,7 +2,7 @@
 
 	// Updates for SA-MP Server Remote Console
 
-	$obj->version = "1.0-beta";
+	$obj->version = "1.0";
 	$obj->releaseDate = "2018-04-19";
 	$obj->description = "Initial release.";
 
--- includes/environment.php
+++ includes/environment.php
@@ -5,7 +5,7 @@
 	$a = (isset($_GET['a'])) ? $_GET['a'] : "";
 	$id = (isset($_GET['id'])) ? (int)$_GET['id'] : 0;
 	$session = (isset($_SESSION['src_logged'])) ? 1 : 0;
-	$version = "1.0-beta";
+	$version = "1.0";
 	$GithubRepo = "https://github.com/Maxelweb/ServerRemoteConsoleSAMP";
 	$lastupdate = "2018-04-19";
 
