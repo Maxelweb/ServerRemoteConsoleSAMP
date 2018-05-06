74bf99766ebce31c3e92fc5738b060eb65dc63d3
Mariano Sciacco <34033090+Maxelweb@users.noreply.github.com>
2018-05-06 03:15:07 +0200

    New Version update

--- includes/environment.php
+++ includes/environment.php
@@ -16,9 +16,9 @@
 	$a = (isset($_GET['a'])) ? $_GET['a'] : "";
 	$id = (isset($_GET['id'])) ? (int)$_GET['id'] : 0;
 	$session = (isset($_SESSION['src_logged'])) ? 1 : 0;
-	$version = "1.1";
+	$version = "1.1.1";
 	$GithubRepo = "https://github.com/Maxelweb/ServerRemoteConsoleSAMP";
-	$lastupdate = "2018-05-01";
+	$lastupdate = "2018-05-06";
 	$errorPermissions = "An error has occured while trying to create an auto-generated config file. Please, make sure to have all necessary permissions in <code>./includes/Config/</code> folder";
 	@define(BULL_FOLDER, "./store/");
 	@define(BULL_EXTENSION, ".txt");
